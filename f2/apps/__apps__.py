43cffbc8ec990627009d38fba87e8da13ca5aa6c
JohnserfSeed <johnserf-seed@foxmail.com>
2023-12-03 23:42:50 +0800

    Create __apps__.py
    app mapping, e.g. tiktok maps to tk

---
+++ f2/apps/__apps__.py
@@ -0,0 +1,23 @@
+_douyin = ["douyin", "dy"]
+_tiktok = ["tiktok", "tk"]
+_weibo = ["weibo", "wb"]
+_youtube = ["youtube", "ytb"]
+_instagram = ["instagram", "ig"]
+_twitter = ["twitter", "x"]
+_bilibili = ["bilibili", "bili"]
+_twitch = ["twitch", "tv"]
+_neteasy_music = ["neteasy_music", "ntm"]
+_little_red_book = ["little_red_book", "lrb"]
+
+__all__ = [
+    "_douyin",
+    "_tiktok",
+    "_weibo",
+    "_youtube",
+    "_instagram",
+    "_twitter",
+    "_bilibili",
+    "_twitch",
+    "_neteasy_music",
+    "_little_red_book",
+]
