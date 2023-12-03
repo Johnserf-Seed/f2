490887058729e935c92429781556671539256cd7
JohnserfSeed <johnserf-seed@foxmail.com>
2023-12-04 00:08:53 +0800

    f2 cli_console
    CustomSpinnerColumn 进度指示器
    ProgressManager 进度管理器
    RichConsoleManager 终端管理器

---
+++ f2/cli/__init__.py
@@ -0,0 +1,5 @@
+# path: f2/cli/__init__.py
+
+from f2.cli.cli_console import CustomSpinnerColumn, ProgressManager, RichConsoleManager
+
+__all__ = ["CustomSpinnerColumn", "ProgressManager", "RichConsoleManager"]
