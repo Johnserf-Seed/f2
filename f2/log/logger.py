bc620f7cdbd18fde333ad5be4c90a69f112f6048
JohnserfSeed <johnserf-seed@foxmail.com>
2023-12-04 00:16:44 +0800

    Create logger.py
    支持定时删除，日志美化，保留日志

---
+++ f2/log/logger.py
@@ -0,0 +1,103 @@
+# path: f2/log/logger.py
+
+import time
+import logging
+import datetime
+
+from pathlib import Path
+from rich.logging import RichHandler
+from f2.utils._singleton import Singleton
+from logging.handlers import TimedRotatingFileHandler
+
+
+class LogManager(metaclass=Singleton):
+    def __init__(self):
+        if getattr(self, "_initialized", False):  # 防止重复初始化
+            return
+
+        self.logger = logging.getLogger("f2")
+        self.logger.setLevel(logging.ERROR)
+        self.log_dir = None
+        self._initialized = True
+
+    def setup_logging(self, level=logging.ERROR, log_to_console=False, log_path=None):
+        self.logger.handlers.clear()
+        self.logger.setLevel(level)
+
+        if log_to_console:
+            ch = RichHandler(
+                show_time=False,
+                show_path=False,
+                markup=True,
+                keywords=(RichHandler.KEYWORDS or []) + ["STREAM"],
+                rich_tracebacks=True,
+            )
+            ch.setFormatter(logging.Formatter("{message}", style="{", datefmt="[%X]"))
+            self.logger.addHandler(ch)
+
+        if log_path:
+            self.log_dir = Path(log_path)
+            self.ensure_log_dir_exists(self.log_dir)
+            log_file_name = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S.log")
+            log_file = self.log_dir.joinpath(log_file_name)
+            fh = TimedRotatingFileHandler(
+                log_file, when="midnight", interval=1, backupCount=99, encoding="utf-8"
+            )
+            fh.setFormatter(
+                logging.Formatter(
+                    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+                )
+            )
+            self.logger.addHandler(fh)
+
+    @staticmethod
+    def ensure_log_dir_exists(log_path: Path):
+        log_path.mkdir(parents=True, exist_ok=True)
+
+    def clean_logs(self, keep_last_n=10):
+        """保留最近的n个日志文件并删除其他文件"""
+        if not self.log_dir:
+            return
+        # self.shutdown()
+        all_logs = sorted(self.log_dir.glob("*.log"))
+        if keep_last_n == 0:
+            files_to_delete = all_logs
+        else:
+            files_to_delete = all_logs[:-keep_last_n]
+        for log_file in files_to_delete:
+            try:
+                log_file.unlink()
+            except PermissionError:
+                self.logger.warning(f"无法删除日志文件 {log_file}, 它正被另一个进程使用")
+
+    def shutdown(self):
+        for handler in self.logger.handlers:
+            handler.close()
+            self.logger.removeHandler(handler)
+        self.logger.handlers.clear()
+        time.sleep(1)  # 确保文件被释放
+
+
+def log_setup():
+    logger = logging.getLogger("f2")
+    if logger.hasHandlers():
+        # logger已经被设置，不做任何操作
+        return logger
+
+    # 创建临时的日志目录
+    temp_log_dir = Path("./logs")
+    temp_log_dir.mkdir(exist_ok=True)
+
+    # 初始化日志管理器
+    log_manager = LogManager()
+    log_manager.setup_logging(
+        level=logging.INFO, log_to_console=False, log_path=temp_log_dir
+    )
+
+    # 只保留99个日志文件
+    log_manager.clean_logs(99)
+
+    return logger
+
+
+logger = log_setup()
