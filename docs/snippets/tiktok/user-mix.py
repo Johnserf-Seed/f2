3c8313168208a8dedbcfc4c32e77773cb0eb22bc
JohnserfSeed <johnserf-seed@foxmail.com>
2024-01-01 01:47:17 +0800

    修正Tk user-mix代码导入与缩进

--- docs/snippets/tiktok/user-mix.py
+++ docs/snippets/tiktok/user-mix.py
@@ -1,14 +1,16 @@
 // #region playlist-sinppet
 import asyncio
-from f2.apps.tiktok.handler import fetch_user_mix_videos
+from f2.apps.tiktok.handler import fetch_user_mix_videos, fetch_play_list
+from f2.apps.tiktok.utils import SecUserIdFetcher
+
 
 async def main():
-    secUid = await SecUserIdFetcher.get_secuid("https://www.tiktok.com/@vantoan___"))
+    secUid = await SecUserIdFetcher.get_secuid("https://www.tiktok.com/@vantoan___")
     playlist = await fetch_play_list(secUid)
 
     for mixId in playlist.get("mixId", []):
         print([
-            aweme_data_list async for aweme_data_list in fetch_user_mix_videos(mix_id)
+            aweme_data_list async for aweme_data_list in fetch_user_mix_videos(mixId)
         ])
 
 if __name__ == "__main__":
@@ -16,21 +18,20 @@ if __name__ == "__main__":
 
 // #endregion playlist-sinppet
 
-
 // #region select-playlist-sinppet
 import asyncio
-from f2.apps.tiktok.handler import fetch_user_mix_videos
+from f2.apps.tiktok.handler import fetch_user_mix_videos, fetch_play_list, select_playlist
 
 async def main():
-    secUid = await SecUserIdFetcher.get_secuid("https://www.tiktok.com/@vantoan___"))
+    secUid = await SecUserIdFetcher.get_secuid("https://www.tiktok.com/@vantoan___")
     playlist = await fetch_play_list(secUid)
 
     selected_index = await select_playlist(playlist)  # [!code focus]
-    if select_index != 0: # [!code focus]
+    if selected_index != 0: # [!code focus]
         mixId = playlist.get("mixId", [])[selected_index - 1] # [!code focus]
 
         print([
-            aweme_data_list async for aweme_data_list in fetch_user_mix_videos(mix_id)
+            aweme_data_list async for aweme_data_list in fetch_user_mix_videos(mixId)
         ])
 
 if __name__ == "__main__":
