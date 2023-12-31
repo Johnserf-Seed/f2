3139258685069d045815e08357b79ac07b6ad638
JohnserfSeed <johnserf-seed@foxmail.com>
2023-12-31 22:20:00 +0800

    文档关于TK aweme-id接口代码片段

---
+++ docs/snippets/tiktok/aweme-id.py
@@ -0,0 +1,38 @@
+// #region single-aweme-id-snippet
+import asyncio
+from f2.apps.tiktok.utils import AwemeIdFetcher
+
+async def main():
+    raw_url = "https://www.tiktok.com/@vantoan___/video/7283528426256911649"
+    # 对于单个URL
+    return await AwemeIdFetcher.get_aweme_id(raw_url)
+
+if __name__ == "__main__":
+    print(asyncio.run(main()))
+
+// #endregion single-aweme-id-snippet
+
+
+// #region multi-aweme-id-snippet
+import asyncio
+from f2.apps.tiktok.utils import AwemeIdFetcher
+from f2.utils.utils import extract_valid_urls
+
+async def main():
+    raw_urls = [
+        "https://www.tiktok.com/@vantoan___/video/7316948869764484384",
+        "https://www.tiktok.com/@vantoan___/video/7316948869764484384?is_from_webapp=1&sender_device=pc&web_id=7306060721837852167",
+        # "https://vt.tiktok.com/xxxxxxxxxx/",
+        "https://www.tiktok.com/t/ZT8mxMcYh/"
+    ]
+
+    # 提取有效URL
+    urls = extract_valid_urls(raw_urls)
+
+    # 对于URL列表
+    return await AwemeIdFetcher.get_all_aweme_id(urls)
+
+if __name__ == "__main__":
+    print(asyncio.run(main()))
+
+// #endregion multi-aweme-id-snippet
