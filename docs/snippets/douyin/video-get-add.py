b29c4e2141fb7db0a1330b38be5061976fdf12f0
JohnserfSeed <johnserf-seed@foxmail.com>
2023-12-31 21:23:25 +0800

    修正抖音video-get-add代码片段导入

--- docs/snippets/douyin/video-get-add.py
+++ docs/snippets/douyin/video-get-add.py
@@ -1,5 +1,6 @@
 import asyncio
 from f2.apps.douyin.handler import get_or_add_video_data
+from f2.apps.douyin.db import AsyncUserDB
 
 # 需要忽略的字段（需过滤掉有时效性的字段）
 ignore_fields = ["video_play_addr", "images", "video_bit_rate", "cover"]
