839a81f69fda384ca2d135827b33be5833240512
JohnserfSeed <johnserf-seed@foxmail.com>
2023-12-31 21:24:39 +0800

    文档关于TK user-profile接口代码片段

---
+++ docs/snippets/tiktok/user-profile.py
@@ -0,0 +1,12 @@
+import asyncio
+from f2.apps.tiktok.handler import handler_user_profile
+
+async def main():
+    secUid = "MS4wLjABAAAAQhcYf_TjRKUku-aF8oqngAfzrYksgGLRz8CKMciBFdfR54HQu3qGs-WoJ-KO7hO8"
+    uniqueId = "vantoan___"
+    print(await handler_user_profile(sec_user_id=sec_user_id))
+    print("-------------------")
+    print(await handler_user_profile(uniqueId=uniqueId))
+
+if __name__ == "__main__":
+    asyncio.run(main())
