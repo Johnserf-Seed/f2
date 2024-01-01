75bf40a0302426e479606d627c4c5d8925e67aee
JohnserfSeed <johnserf-seed@foxmail.com>
2024-01-01 14:39:56 +0800

    v0.0.1.1-pw1
    [Fixed]
    
    修复pyproject.toml依赖部分遗漏造成的`Error: No such command` https://github.com/Johnserf-Seed/TikTokDownload/issues/623

--- f2/__init__.py
+++ f2/__init__.py
@@ -1,5 +1,5 @@
 __author__  = "JohnserfSeed <johnserf-seed@foxmail.com>"
-__version__ = "0.0.1.0"
+__version__ = "0.0.1.1"
 __description_cn__ = '基于[red]异步[/red]的[green]全平台下载工具.'
 __description_en__ = '[yellow]Asynchronous based [/yellow]full-platform download tool.'
 __reponame__ = 'f2'
