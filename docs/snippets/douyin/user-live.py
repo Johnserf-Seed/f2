b33e79cb7748b3cd8ee784fd9084f11ec91dfabc
JohnserfSeed <johnserf-seed@foxmail.com>
2024-01-04 20:30:03 +0800

    修正抖音user-live代码片段方法名错误

--- docs/snippets/douyin/user-live.py
+++ docs/snippets/douyin/user-live.py
@@ -2,7 +2,7 @@ import asyncio
 from f2.apps.douyin.handler import fetch_user_live_videos
 
 async def main():
-    print(await fetch_user_collect_videos(webcast_id="775841227732"))
+    print(await fetch_user_live_videos(webcast_id="775841227732"))
 
 if __name__ == "__main__":
     asyncio.run(main())
