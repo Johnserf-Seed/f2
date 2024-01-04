e20cd735002a00e945483f0a8578dcb95114e377
JohnserfSeed <johnserf-seed@foxmail.com>
2024-01-04 20:30:11 +0800

    修正抖音user-mix代码片段aweme_id不明的问题

--- docs/snippets/douyin/user-mix.py
+++ docs/snippets/douyin/user-mix.py
@@ -1,8 +1,8 @@
 import asyncio
-from f2.apps.douyin.handler import fetch_one_video,fetch_user_mix_videos
+from f2.apps.douyin.handler import fetch_one_video, fetch_user_mix_videos
 
 async def main():
-    mix_id = await fetch_one_video(aweme_id).get("mix_id")
+    mix_id = await fetch_one_video(aweme_id="7294914031133969705").get("mix_id")
     results = [
         aweme_data_list async for aweme_data_list in fetch_user_collect_videos(mix_id)
     ]
