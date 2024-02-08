985fd187abc4b3d1ee00bae97020fbfdc6eee99c
JohnserfSeed <johnserf-seed@foxmail.com>
2024-02-08 17:11:52 +0800

    更新 `get_or_add_user_data` 方法，以处理 `UserProfileFilter`

--- f2/apps/douyin/handler.py
+++ f2/apps/douyin/handler.py
@@ -121,7 +121,7 @@ class DouyinHandler:
 
         # 如果用户不在数据库中，将其添加到数据库
         if not local_user_data:
-            await db.add_user_info(**current_user_data)
+            await db.add_user_info(**current_user_data._to_dict())
 
         return user_path
 
