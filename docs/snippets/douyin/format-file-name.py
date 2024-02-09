e466446e5536c81ebee832bb86b97cfc96fad724
JohnserfSeed <johnserf-seed@foxmail.com>
2024-02-09 22:39:02 +0800

    更新douyin `format-file-name`文档代码片段

--- docs/snippets/douyin/format-file-name.py
+++ docs/snippets/douyin/format-file-name.py
@@ -1,20 +1,30 @@
 import asyncio
-from f2.apps.douyin.handler import fetch_one_video
+from f2.apps.douyin.handler import DouyinHandler
 from f2.apps.douyin.utils import format_file_name
 
 
 async def main():
     # 文件名模板
-    kwargs = {"naming": "{create}_{desc}_{aweme_id}"}
+    kwargs = {
+        "proxies": {"http": None, "https": None},
+        "naming": "{create}_{desc}_{aweme_id}",
+        "cookie": "",
+    }
     # 单作品的数据
-    aweme_data = await fetch_one_video("7294994585925848359")
+    aweme_data = await DouyinHandler(kwargs).fetch_one_video("7218193198328433954")
     # 格式化后的文件名
     print(format_file_name(kwargs.get("naming"), aweme_data) + "_video")
 
+    # 文件名模板
+    kwargs = {
+        "proxies": {"http": None, "https": None},
+        "naming": "{create}_{desc}_{aweme_id}_{location}",
+        "cookie": "",
+    }
     # 用户自定义字段
     custom_fields = {"location": "New York"}
     # 格式化后的自定义文件名
-    print((kwargs.get("naming"), aweme_data, custom_fields) + "_video")
+    print(format_file_name(kwargs.get("naming"), aweme_data, custom_fields) + "_video")
     # 格式化后的自定义文件名，长度限制在100
     print(
         format_file_name(kwargs.get("naming"), aweme_data, custom_fields, 100)
