08d67529bfb950de56b7499086b2ee78be104f81
JohnserfSeed <johnserf-seed@foxmail.com>
2024-02-14 18:17:05 +0800

    修正tiktok 示例代码片段

--- examples/tiktok/userProfile.py
+++ examples/tiktok/userProfile.py
@@ -1,14 +1,23 @@
 import asyncio
-from f2.apps.tiktok.handler import handler_user_profile
+from f2.apps.tiktok.handler import TiktokHandler
+
+kwargs = {
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+        "Referer": "https://www.tiktok.com/",
+    },
+    "proxies": {"http": None, "https": None},
+    "cookie": "YOUR_COOKIE_HERE",
+}
 
 
 async def main():
-    user = await handler_user_profile(
+    user = await TiktokHandler(kwargs).handler_user_profile(
         secUid="MS4wLjABAAAAQhcYf_TjRKUku-aF8oqngAfzrYksgGLRz8CKMciBFdfR54HQu3qGs-WoJ-KO7hO8"
     )
     print(user)
     print("-------------------")
-    user = await handler_user_profile(uniqueId="sophia.ilysm")
+    user = await TiktokHandler(kwargs).handler_user_profile(uniqueId="sophia.ilysm")
     print(user)
 
 
