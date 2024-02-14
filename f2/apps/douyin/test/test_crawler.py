591b309bd3331741d1b6cd7c13d8c710bcabcdfc
JohnserfSeed <johnserf-seed@foxmail.com>
2024-02-14 16:17:13 +0800

    更新douyin单元测试用例

--- f2/apps/douyin/test/test_crawler.py
+++ f2/apps/douyin/test/test_crawler.py
@@ -4,9 +4,18 @@ from f2.apps.douyin.filter import UserPostFilter
 from f2.apps.douyin.crawler import DouyinCrawler
 
 
+kwargs = {
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+        "Referer": "https://www.douyin.com/",
+    },
+    "cookie": "YOUR_COOKIE_HERE",
+}
+
+
 @pytest.mark.asyncio
 async def test_crawler_fetcher():
-    async with DouyinCrawler() as crawler:
+    async with DouyinCrawler(kwargs) as crawler:
         params = UserPost(
             max_cursor=0,
             count=1,
