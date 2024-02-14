4687893a8f0b1f76caea0cfd65f12d32660b301a
JohnserfSeed <johnserf-seed@foxmail.com>
2024-02-14 17:26:45 +0800

    更新tiktok 文档开发者接口代码片段
    修正一些错误以及适配新接口

--- docs/snippets/tiktok/video-get-add.py
+++ docs/snippets/tiktok/video-get-add.py
@@ -1,15 +1,28 @@
 import asyncio
-from f2.apps.tiktok.handler import fetch_one_video, get_or_add_video_data
+from f2.apps.tiktok.handler import TiktokHandler
 from f2.apps.tiktok.db import AsyncVideoDB
 
 # 需要忽略的字段（需过滤掉有时效性的字段）
 ignore_fields = ["video_play_addr", "images", "video_bit_rate", "cover"]
 
+kwargs = {
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+        "Referer": "https://www.tiktok.com/",
+    },
+    "proxies": {"http": None, "https": None},
+    "cookie": "YOUR_COOKIE_HERE",
+}
+
 
 async def main():
-    aweme_data = await fetch_one_video(itemId="7095819783324601605")
+    aweme_data = await TiktokHandler(kwargs).fetch_one_video(
+        itemId="7095819783324601605"
+    )
     async with AsyncVideoDB("douyin_videos.db") as avdb:
-        await get_or_add_video_data(aweme_data, avdb, ignore_fields)
+        await TiktokHandler(kwargs).get_or_add_video_data(
+            aweme_data, avdb, ignore_fields
+        )
 
 
 if __name__ == "__main__":
