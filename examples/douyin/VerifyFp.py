042967a319c128b9082b14cfdec03e134384ab36
JohnserfSeed <johnserf-seed@foxmail.com>
2024-02-14 18:16:30 +0800

    注释与部分代码修改
    black格式化
    修正一些错误

--- examples/douyin/VerifyFp.py
+++ examples/douyin/VerifyFp.py
@@ -1,4 +1,4 @@
 from f2.apps.douyin.utils import VerifyFpManager
 
-if __name__ == '__main__':
-    print('verify_fp:', VerifyFpManager.gen_verify_fp())
+if __name__ == "__main__":
+    print("verify_fp:", VerifyFpManager.gen_verify_fp())
