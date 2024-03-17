f5bb6485b35ed371f9ec68e8215eb16dfe876768
JohnserfSeed <johnserf-seed@foxmail.com>
2024-03-17 16:27:28 +0800

    del: 删除example示例
    请参考docs下的代码片段

--- examples/douyin/msToken.py
+++
@@ -1,5 +0,0 @@
-from f2.apps.douyin.utils import TokenManager
-
-if __name__ == '__main__':
-    print('douyin real msToken:', TokenManager.gen_real_msToken())
-    print('douyin fake msToken:', TokenManager.gen_false_msToken())
--- examples/tiktok/ttwid.py
+++
@@ -1,4 +0,0 @@
-from f2.apps.tiktok.utils import TokenManager
-
-if __name__ == "__main__":
-    print("tiktok ttwid:", TokenManager.gen_ttwid())
