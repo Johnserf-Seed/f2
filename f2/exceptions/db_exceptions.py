e57a8077714796eeba4cc5bd6ce5d676d1b9ad39
JohnserfSeed <johnserf-seed@foxmail.com>
2024-04-04 18:08:13 +0800

    refactor: 重构数据库异常类

--- f2/exceptions/db_exceptions.py
+++ f2/exceptions/db_exceptions.py
@@ -8,46 +8,48 @@ exception_console = RichConsoleManager().exception_console
 class DatabaseError(Exception):
     """基本数据库异常类，其他数据库异常都会继承这个类"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, message=None, db=None):
         exception_console.print(
             "请前往QA文档 https://johnserf-seed.github.io/f2/question-answer/qa.html 查看相关帮助"
         )
+        self.db = db
+        super().__init__(message)
 
-    def display_error(self):
-        """显示错误信息"""
-        return f"Database Error: {self.args[0]}."
+    def __str__(self):
+        """返回错误信息和db（如果有的话）"""
+        return f"{super().__str__()}" + (f" Database: {self.db}" if self.db else "")
 
 
 class DatabaseConnectionError(DatabaseError):
     """当与数据库的连接出现问题时抛出"""
 
-    def display_error(self):
-        return f"Database Connection Error: {self.args[0]}."
+    def __init__(self, message=None, db=None):
+        super().__init__(message, db)
 
 
 class RecordNotFoundError(DatabaseError):
     """当在数据库中找不到预期的记录时抛出"""
 
-    def display_error(self):
-        return f"Record Not Found Error: {self.args[0]}."
+    def __init__(self, message=None, db=None):
+        super().__init__(message, db)
 
 
 class MultipleRecordsFoundError(DatabaseError):
     """当期望找到一个记录但实际找到多个时抛出"""
 
-    def display_error(self):
-        return f"Multiple Records Found Error: {self.args[0]}."
+    def __init__(self, message=None, db=None):
+        super().__init__(message, db)
 
 
 class DatabaseTimeoutError(DatabaseError):
     """当数据库操作超时时抛出"""
 
-    def display_error(self):
-        return f"Database Timeout Error: {self.args[0]}."
+    def __init__(self, message=None, db=None):
+        super().__init__(message, db)
 
 
 class DatabaseConstraintError(DatabaseError):
     """当违反数据库约束时抛出，例如唯一性约束"""
 
-    def display_error(self):
-        return f"Database Constraint Error: {self.args[0]}."
+    def __init__(self, message=None, db=None):
+        super().__init__(message, db)
