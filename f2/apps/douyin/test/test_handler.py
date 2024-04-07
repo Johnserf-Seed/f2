d228c687aa93bc22e0ae91478d78e98c608afc2a
JohnserfSeed <johnserf-seed@foxmail.com>
2024-04-07 18:25:59 +0800

    test: 修改douyin测试用例

--- f2/apps/douyin/test/test_handler.py
+++ f2/apps/douyin/test/test_handler.py
@@ -1,24 +1,21 @@
 import pytest
 from f2.apps.douyin.handler import DouyinHandler
+from f2.utils.conf_manager import TestConfigManager
 
 
-kwargs = {
-    "headers": {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-        "Referer": "https://www.douyin.com/",
-    },
-    "cookie": "YOUR_COOKIE_HERE",
-}
-user_sec_id = "MS4wLjABAAAANXSltcLCzDGmdNFI2Q_QixVTr67NiYzjKOIP5s03CAE"
+@pytest.fixture
+def cookie_fixture():
+    return TestConfigManager.get_test_config("douyin")
 
 
 @pytest.mark.asyncio
-async def test_fetch_user_post_videos():
-    results = [
+async def test_fetch_user_post_videos(cookie_fixture):
+    async for aweme_data_list in DouyinHandler(cookie_fixture).fetch_user_post_videos(
+        sec_user_id="MS4wLjABAAAANXSltcLCzDGmdNFI2Q_QixVTr67NiYzjKOIP5s03CAE",
+        max_cursor=0,
+        page_counts=1,
+        max_counts=1,
+    ):
         aweme_data_list
-        async for aweme_data_list in DouyinHandler(kwargs).fetch_user_post_videos(
-            user_sec_id
-        )
-    ]
 
-    assert results, f"Failed to fetch videos for user_sec_id: {user_sec_id}"
+    assert aweme_data_list, f"Failed to fetch videos for user_sec_id: {aweme_data_list}"
