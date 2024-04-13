baa5d2dd74f46e679f47b55eab38716260edcde6
JohnserfSeed <johnserf-seed@foxmail.com>
2024-04-13 15:25:56 +0800

    test: 修改tiktok测试代理配置

--- docs/snippets/tiktok/one-video.py
+++ docs/snippets/tiktok/one-video.py
@@ -6,7 +6,7 @@ kwargs = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
         "Referer": "https://www.tiktok.com/",
     },
-    "proxies": {"http": None, "https": None},
+    "proxies": {"http://": None, "https://": None},
     "cookie": "YOUR_COOKIE_HERE",
 }
 
