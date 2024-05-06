d44f71f458a855a5310d69aca8794e889845ffbc
JohnserfSeed <johnserf-seed@foxmail.com>
2024-05-06 14:49:08 +0800

    tests: 添加douyin查询用户代码片段

---
+++ docs/snippets/douyin/query-user.py
@@ -0,0 +1,26 @@
+import asyncio
+from f2.apps.douyin.handler import DouyinHandler
+from f2.apps.douyin.utils import TokenManager
+
+kwargs = {
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
+        "Referer": "https://www.douyin.com/",
+    },
+    "proxies": {"http://": None, "https://": None},
+    "timeout": 10,
+    "cookie": f"ttwid={TokenManager.gen_ttwid()}",
+}
+
+
+async def main():
+
+    user = await DouyinHandler(kwargs).fetch_query_user()
+    print("=================_to_raw================")
+    print(user._to_raw())
+    # print("=================_to_dict===============")
+    # print(aweme_data_list._to_dict())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
