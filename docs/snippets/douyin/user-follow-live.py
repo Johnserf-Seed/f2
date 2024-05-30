e9f84c805ee7b5e9bb15e4a7270588d1837c9be4
JohnserfSeed <johnserf-seed@foxmail.com>
2024-05-30 19:46:54 +0800

    test: 添加douyin关注用户直播代码片段

---
+++ docs/snippets/douyin/user-follow-live.py
@@ -0,0 +1,26 @@
+import asyncio
+from f2.apps.douyin.handler import DouyinHandler
+
+
+kwargs = {
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+        "Referer": "https://www.douyin.com/",
+    },
+    "proxies": {"http://": None, "https://": None},
+    "cookie": "YOUR_COOKIE_HERE",
+}
+
+
+async def main():
+    follow_live = await DouyinHandler(kwargs).fetch_following_live()
+    print("=================_to_raw================")
+    print(follow_live._to_raw())
+    # print("=================_to_dict===============")
+    # print(follow_live._to_dict())
+    # print("=================_to_list===============")
+    # print(follow_live._to_list())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
