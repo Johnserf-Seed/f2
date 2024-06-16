6d0f0e74681b1f2b6b96a559da5e948e2d70eb58
JohnserfSeed <johnserf-seed@foxmail.com>
2024-06-16 21:59:51 +0800

    perf: 修改tiktok设备id代码片段变量名

--- docs/snippets/tiktok/device-id.py
+++ docs/snippets/tiktok/device-id.py
@@ -3,10 +3,10 @@ from f2.apps.tiktok.utils import DeviceIdManager
 
 
 async def main():
-    devuce_id = await DeviceIdManager.gen_device_id()
-    print(devuce_id)
-    devuce_id = await DeviceIdManager.gen_device_id(full_cookie=True)
-    print(devuce_id)
+    device_id = await DeviceIdManager.gen_device_id()
+    print(device_id)
+    device_id = await DeviceIdManager.gen_device_id(full_cookie=True)
+    print(device_id)
     device_ids = await DeviceIdManager.gen_device_ids(3)
     print(device_ids)
     device_ids = await DeviceIdManager.gen_device_ids(3, full_cookie=True)
