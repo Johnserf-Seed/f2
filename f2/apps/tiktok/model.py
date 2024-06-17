a4ba64182f27b87fc11fcfc0406ccb72109f490b
JohnserfSeed <johnserf-seed@foxmail.com>
2024-06-17 21:10:41 +0800

    perf: 更新配置与模型的ua参数

--- f2/apps/tiktok/model.py
+++ f2/apps/tiktok/model.py
@@ -21,7 +21,7 @@ class BaseRequestModel(BaseModel):
     browser_version: str = quote(
         ClientConfManager.brm_browser().get(
             "version",
-            "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+            "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
         ),
         safe="",
     )
