c64ebc2f4d3f70ac0fbc436d332de74f5755db05
JohnserfSeed <johnserf-seed@foxmail.com>
2024-11-15 15:08:16 +0800

    perf: 更新app的BaseCrawler初始化参数

--- f2/apps/weibo/crawler.py
+++ f2/apps/weibo/crawler.py
@@ -21,7 +21,7 @@ class WeiboCrawler(BaseCrawler):
         # 需要与cli同步
         proxies = kwargs.get("proxies", {"http://": None, "https://": None})
         self.headers = kwargs.get("headers") | {"Cookie": kwargs["cookie"]}
-        super().__init__(proxies=proxies, crawler_headers=self.headers)
+        super().__init__(kwargs, proxies=proxies, crawler_headers=self.headers)
 
     async def fetch_user_info(self, params: UserInfo):
         endpoint = ModelManager.model_2_endpoint(
