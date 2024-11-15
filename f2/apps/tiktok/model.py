4f373ba46e73fb2debb77c0218663b8f66d84358
JohnserfSeed <johnserf-seed@foxmail.com>
2024-11-15 17:05:14 +0800

    feat: 添加tiktok基础直播间接口模型

--- f2/apps/tiktok/model.py
+++ f2/apps/tiktok/model.py
@@ -62,6 +62,36 @@ class BaseRequestModel(BaseModel):
         msToken: str = TokenManager.gen_real_msToken()
 
 
+class BaseWebCastModel(BaseModel):
+    aid: str = "1988"
+    app_language: str = "zh-Hans"
+    app_name: str = "tiktok_web"
+    browser_language: str = ClientConfManager.brm_browser().get("language", "zh-CN")
+    browser_name: str = ClientConfManager.brm_browser().get("name", "Mozilla")
+    browser_online: str = "true"
+    browser_platform: str = ClientConfManager.brm_browser().get("platform", "Win32")
+    browser_version: str = quote(
+        ClientConfManager.brm_browser().get(
+            "version",
+            "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0",
+        ),
+        safe="",
+    )
+    cookie_enabled: str = "true"
+    debug: str = "false"
+    device_platform: str = "web"
+    host: str = quote("https://webcast.tiktok.com", safe="")
+    identity: str = "audience"
+    live_id: int = 12
+    screen_height: int = 1080
+    screen_width: int = 1920
+    sup_ws_ds_opt: int = 1
+    tz_name: str = quote(
+        ClientConfManager.base_request_model().get("tz_name", "Asia/Hong_Kong"), safe=""
+    )
+    version_code: str = "270000"
+
+
 # router model
 class UserProfile(BaseRequestModel):
     secUid: str
