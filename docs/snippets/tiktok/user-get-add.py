ae5dda3b77058a0ff33d8ccc323a06266a403820
JohnserfSeed <johnserf-seed@foxmail.com>
2024-11-19 16:10:40 +0800

    perf: 更新ua版本126 -> 130

--- docs/snippets/tiktok/user-get-add.py
+++ docs/snippets/tiktok/user-get-add.py
@@ -4,7 +4,7 @@ from f2.apps.tiktok.db import AsyncUserDB
 
 kwargs = {
     "headers": {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0",
         "Referer": "https://www.tiktok.com/",
     },
     "proxies": {"http://": None, "https://": None},
--- f2/apps/douyin/algorithm/webcast_signature.js
+++ f2/apps/douyin/algorithm/webcast_signature.js
@@ -43,7 +43,7 @@
 _window = global;
 _document = {};
 // _navigator = {
-//     userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
+//     userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0"
 // }
 _history = {};
 _screen = {};
