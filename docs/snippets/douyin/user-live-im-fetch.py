af0e9a56f16fb8bfcbf7de89551854a001e1dda4
JohnserfSeed <johnserf-seed@foxmail.com>
2024-11-21 17:44:11 +0800

    perf: 更新ua版本 126 -> 130

--- docs/snippets/douyin/user-live-im-fetch.py
+++ docs/snippets/douyin/user-live-im-fetch.py
@@ -7,7 +7,7 @@ from f2.log.logger import logger
 
 kwargs = {
     "headers": {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0",
         "Referer": "https://www.douyin.com/",
         "Content-Type": "application/protobuffer;",
     },
@@ -19,7 +19,7 @@ kwargs = {
 
 kwargs2 = {
     "headers": {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0",
         "Upgrade": "websocket",
         "Connection": "Upgrade",
     },
