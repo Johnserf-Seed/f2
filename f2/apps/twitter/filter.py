e3aab19967322fb72c0d0dcd1a95a7c40c249ce5
JohnserfSeed <johnserf-seed@foxmail.com>
2024-11-28 17:36:33 +0800

    feat: 添加twitter书签推文数据过滤器

--- f2/apps/twitter/filter.py
+++ f2/apps/twitter/filter.py
@@ -668,3 +668,292 @@ class LikeTweetFilter(PostTweetFilter):
 
     def __init__(self, data):
         super().__init__(data)
+
+
+class BookmarkTweetFilter(JSONModel):
+
+    # 用户发布的推文__typename是TweetWithVisibilityResults
+    @property
+    def cursorType(self):
+        return self._get_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[-1].content.cursorType"
+        )
+
+    @property
+    def min_cursor(self):
+        return self._get_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[-2].content.value"
+        )
+
+    @property
+    def max_cursor(self):
+        return self._get_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[-1].content.value"
+        )
+
+    @property
+    def entryId(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].entryId"
+        )
+
+    # tweet
+    @property
+    def tweet_id(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.legacy.conversation_id_str"
+        )
+
+    @property
+    def tweet_created_at(self):
+        create_times = self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.legacy.created_at"
+        )
+        return (
+            [timestamp_2_str(str(ct)) for ct in create_times]
+            if isinstance(create_times, list)
+            else timestamp_2_str(str(create_times))
+        )
+
+    @property
+    def tweet_favorite_count(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.legacy.favorite_count"
+        )
+
+    @property
+    def tweet_reply_count(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.legacy.reply_count"
+        )
+
+    @property
+    def tweet_retweet_count(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.legacy.retweet_count"
+        )
+
+    @property
+    def tweet_quote_count(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.legacy.quote_count"
+        )
+
+    @property
+    def tweet_views_count(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.views.count"
+        )
+
+    @property
+    def tweet_desc(self):
+        text_list = self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.legacy.full_text"
+        )
+
+        return replaceT(
+            [
+                extract_desc(text) if text and isinstance(text, str) else ""
+                for text in text_list
+            ]
+        )
+
+    @property
+    def tweet_desc_raw(self):
+        text_list = self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.legacy.full_text"
+        )
+
+        return [
+            extract_desc(text) for text in text_list if text and isinstance(text, str)
+        ]
+
+    @property
+    def tweet_media_status(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.legacy.entities.media[*].ext_media_availability.status"
+        )
+
+    @property
+    def tweet_media_type(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.legacy.entities.media[0].type"
+        )
+
+    @property
+    def tweet_media_url(self):
+        media_lists = self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.legacy.entities.media"
+        )
+
+        return [
+            (
+                [
+                    media["media_url_https"]
+                    for media in media_list
+                    if isinstance(media, dict) and "media_url_https" in media
+                ]
+                if media_list
+                else None
+            )
+            for media_list in media_lists
+        ]
+
+    @property
+    def tweet_video_url(self):
+
+        video_url_lists = self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.legacy.entities.media"
+        )
+
+        return [
+            (
+                [
+                    video_url["video_info"]["variants"][-1]["url"]
+                    for video_url in video_url_list
+                    if isinstance(video_url, dict) and "video_info" in video_url
+                ]
+                if video_url_list
+                else None
+            )
+            for video_url_list in video_url_lists
+        ]
+
+    # user
+    @property
+    def user_id(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.id"
+        )
+
+    @property
+    def is_blue_verified(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.is_blue_verified"
+        )
+
+    @property
+    def user_created_at(self):
+        create_times = self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.created_at"
+        )
+        return (
+            [timestamp_2_str(str(ct)) for ct in create_times]
+            if isinstance(create_times, list)
+            else timestamp_2_str(str(create_times))
+        )
+
+    @property
+    def user_description(self):
+        return replaceT(
+            self._get_list_attr_value(
+                "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.description"
+            )
+        )
+
+    @property
+    def user_description_raw(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.description"
+        )
+
+    @property
+    def user_location(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.location"
+        )
+
+    @property
+    def user_friends_count(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.friends_count"
+        )
+
+    @property
+    def user_followers_count(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.followers_count"
+        )
+
+    @property
+    def user_favourites_count(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.favourites_count"
+        )
+
+    @property
+    def user_media_count(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.media_count"
+        )
+
+    @property
+    def user_statuses_count(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.statuses_count"
+        )
+
+    @property
+    def nickname(self):
+        return replaceT(
+            self._get_list_attr_value(
+                "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.name"
+            )
+        )
+
+    @property
+    def nickname_raw(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.name"
+        )
+
+    @property
+    def user_screen_name(self):
+        return replaceT(
+            self._get_list_attr_value(
+                "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.screen_name"
+            )
+        )
+
+    @property
+    def user_screen_name_raw(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.screen_name"
+        )
+
+    @property
+    def user_profile_banner_url(self):
+        return self._get_list_attr_value(
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries[*].content.itemContent.tweet_results.result.core.user_results.result.legacy.profile_banner_url"
+        )
+
+    def _to_raw(self) -> dict:
+        return self._data
+
+    def _to_dict(self) -> dict:
+        return {
+            prop_name: getattr(self, prop_name)
+            for prop_name in dir(self)
+            if not prop_name.startswith("__") and not prop_name.startswith("_")
+        }
+
+    def _to_list(self) -> list:
+        exclude_fields = [
+            "max_cursor",
+            "min_cursor",
+            "cursorType",
+        ]
+
+        extra_fields = [
+            "max_cursor",
+            "min_cursor",
+        ]
+
+        list_dicts = filter_to_list(
+            self,
+            "$.data.bookmark_timeline_v2.timeline.instructions[-1].entries",
+            exclude_fields,
+            extra_fields,
+        )
+
+        return list_dicts
