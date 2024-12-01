7458b0af66387e8d0293ff460a99cd101848bccc
JohnserfSeed <johnserf-seed@foxmail.com>
2024-12-01 21:32:54 +0800

    docs: 添加weibo代码片段

--- docs/snippets/weibo/user-detail.py
+++ docs/snippets/weibo/user-detail.py
@@ -14,9 +14,10 @@ kwargs = {
 
 
 async def main():
-    user = await WeiboHandler(kwargs).fetch_user_info(uid="2265830070")
+    uid = "2265830070"
+    user = await WeiboHandler(kwargs).fetch_user_detail(uid=uid)
     logger.info(
-        f"微博用户ID: {user.user_id}, 昵称: {user.nickname}, 性别: {user.gender}, 地区: {user.location}, 关注数: {user.friends_count}, 粉丝数: {user.followers_count}, 微博数: {user.weibo_count}, 个人主页: {user.profile_url}"
+        f"用户ID: {uid}, 生日: {user.birthday}, 性别: {user.gender}, 地区: {user.location}, 个性签名: {user.description_raw}, 注册时间: {user.create_at}, 视频播放次数: {user.video_play_count}"
     )
 
 
