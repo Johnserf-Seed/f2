634a3d37ee0b3c44515a0569fabee693fc3a3d3f
JohnserfSeed <johnserf-seed@foxmail.com>
2024-12-01 21:32:54 +0800

    docs: 添加weibo代码片段

--- docs/snippets/weibo/user-profile-by-name.py
+++ docs/snippets/weibo/user-profile-by-name.py
@@ -14,7 +14,9 @@ kwargs = {
 
 
 async def main():
-    user = await WeiboHandler(kwargs).fetch_user_info(uid="2265830070")
+    user = await WeiboHandler(kwargs).fetch_user_info_by_screen_name(
+        screen_name="阿里多多酱"
+    )
     logger.info(
         f"微博用户ID: {user.user_id}, 昵称: {user.nickname}, 性别: {user.gender}, 地区: {user.location}, 关注数: {user.friends_count}, 粉丝数: {user.followers_count}, 微博数: {user.weibo_count}, 个人主页: {user.profile_url}"
     )
