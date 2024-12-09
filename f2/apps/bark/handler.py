0049459b90af6513ab73a4c5fbaa1040957a0e20
JohnserfSeed <johnserf-seed@foxmail.com>
2024-12-09 20:39:21 +0800

    feat: 添加Bark加密推送方法

--- f2/apps/bark/handler.py
+++ f2/apps/bark/handler.py
@@ -1,13 +1,18 @@
 # path: f2/apps/bark/handler.py
 
+import json
+
 from typing import Dict
+from base64 import b64encode
 
 from f2.log.logger import logger
 from f2.i18n.translator import _
 from f2.utils.decorators import mode_handler, mode_function_map
+from f2.utils.utils import AESEncryptionUtils
 from f2.apps.bark.crawler import BarkCrawler
-from f2.apps.bark.model import BarkModel
+from f2.apps.bark.model import BarkModel, BarkCipherModel
 from f2.apps.bark.filter import BarkNotificationFilter
+from f2.apps.bark.utils import generate_numeric_bytes
 
 
 class BarkHandler:
@@ -26,7 +31,7 @@ class BarkHandler:
         Returns:
             BarkNotificationFilter: 处理后的Bark通知过滤结果
         """
-        logger.info(_("正在发送Bark通知"))
+        logger.info(_("正在发送 Bark 通知"))
 
         # 获取并确保 body 存在
         self.kwargs["body"] = self.kwargs.get("body", "无内容")
@@ -66,6 +71,80 @@ class BarkHandler:
     async def post_bark_notification(self) -> BarkNotificationFilter:
         """用于发送Bark通知 (post 方式)"""
         return await self._send_bark_notification("post")
+
+    @mode_handler("cipher")
+    async def cipher_bark_notification(self) -> BarkNotificationFilter:
+        """用于发送加密 Bark 通知"""
+
+        logger.info(_("正在发送 Bark 加密通知"))
+
+        # 获取 Bark 加密设置参数
+        encryption = self.kwargs.get("encryption")
+        if not encryption:
+            raise ValueError(_("Bark 加密配置缺失"))
+
+        # 获取并检查加密算法相关参数
+        aes_algo = encryption.get("algorithm")
+        if not aes_algo:
+            raise ValueError(_("加密算法缺失"))
+
+        aes_mode = encryption.get("mode")
+        if not aes_mode:
+            raise ValueError(_("加密模式缺失"))
+
+        aes_padding = encryption.get("padding")
+        aes_key = encryption.get("key")
+        if not aes_key:
+            raise ValueError(_("加密密钥缺失"))
+
+        aes_key = aes_key.encode("utf-8")
+
+        # 根据加密模式生成不同位数的 IV
+        if aes_mode == "ECB":
+            aes_iv = None
+        elif aes_mode == "CBC":
+            aes_iv = generate_numeric_bytes(16)
+        elif aes_mode == "GCM":
+            aes_iv = generate_numeric_bytes(12)
+        else:
+            raise ValueError(_("无效的加密模式：{0}").format(aes_mode))
+
+        # 初始化 AES 加密工具并校验参数
+        aes = AESEncryptionUtils(
+            key=aes_key,
+            algorithm=aes_algo,
+            mode=aes_mode,
+            padding_scheme=aes_padding,
+            iv=aes_iv,
+        )
+
+        try:
+            async with BarkCrawler(self.kwargs) as crawler:
+                # 对原始 params 进行加密
+                plaintext = json.dumps(
+                    BarkModel(**self.kwargs).model_dump(), ensure_ascii=False
+                ).encode("utf-8")
+                encrypted_params = aes.aes_encrypt(plaintext)
+
+                # 需要将随机生成的 IV 一并发送
+                cipher_params = BarkCipherModel(
+                    ciphertext=b64encode(encrypted_params).decode("utf-8"),
+                    iv="".join(chr(b) for b in aes.iv) if aes.iv else "",
+                )
+
+                logger.debug(cipher_params.model_dump())
+
+                response = await crawler.cipher_bark_notification(cipher_params)
+                bark = BarkNotificationFilter(response)
+
+                logger.info(
+                    _("Bark 加密通知发送成功，内容：{0}，时间：{1}").format(
+                        self.kwargs["body"], bark.timestamp
+                    )
+                )
+                return bark
+        except Exception as e:
+            logger.error(_("Bark 通知发送失败，请检查 key 和网络连接：{0}").format(e))
 
     async def send_quick_notification(
         self,
