f86c67232f89cdc4c0908d6f8657b281b24f007c
JohnserfSeed <johnserf-seed@foxmail.com>
2024-12-11 02:34:43 +0800

    docs: 添加默认主题的视图过渡效果

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,11 @@
 
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
 import './styles/vars.css'
 
 
 export default {
   extends: DefaultTheme,
+  Layout: Layout,
 }
