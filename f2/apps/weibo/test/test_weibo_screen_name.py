96ae30533dbbb6b3b47b8e26db53d0877477db0c
JohnserfSeed <johnserf-seed@foxmail.com>
2024-12-11 22:26:17 +0800

    feat: 添加weibo列表用户昵称提取方法与测试用例

--- f2/apps/weibo/test/test_weibo_screen_name.py
+++ f2/apps/weibo/test/test_weibo_screen_name.py
@@ -71,3 +71,42 @@ class TestWeiboScreenNameFetcher:
         weibo_name_url = "http://weibo.com/自我充电功能丧失"
         with pytest.raises(APINotFoundError):
             await WeiboScreenNameFetcher.get_weibo_screen_name(weibo_name_url)
+
+
+@pytest.mark.asyncio
+class TestWeiboAllScreenNameFetcher:
+    async def test_get_weibo_all_screen_name(self):
+        weibo_urls = [
+            "https://weibo.com/n/%E8%87%AA%E6%88%91%E5%85%85%E7%94%B5%E5%8A%9F%E8%83%BD%E4%B8%A7%E5%A4%B1",
+            "https://weibo.com/n/%E8%87%AA%E6%88%91%E5%85%85%E7%94%B5%E5%8A%9F%E8%83%BD%E4%B8%A7%E5%A4%B1/",
+            "https://weibo.com/n/%E8%87%AA%E6%88%91%E5%85%85%E7%94%B5%E5%8A%9F%E8%83%BD%E4%B8%A7%E5%A4%B1?test=123",
+            "https://weibo.com/n/%E8%87%AA%E6%88%91%E5%85%85%E7%94%B5%E5%8A%9F%E8%83%BD%E4%B8%A7%E5%A4%B1/?test=123",
+            "https://weibo.com/n/自我充电功能丧失",
+            "https://weibo.com/n/自我充电功能丧失/",
+            "https://weibo.com/n/自我充电功能丧失?test=123",
+            "https://weibo.com/n/自我充电功能丧失/?test=123",
+        ]
+
+        weibo_names = await WeiboScreenNameFetcher.get_all_weibo_screen_name(weibo_urls)
+
+        assert weibo_names == [
+            "自我充电功能丧失",
+            "自我充电功能丧失",
+            "自我充电功能丧失",
+            "自我充电功能丧失",
+            "自我充电功能丧失",
+            "自我充电功能丧失",
+            "自我充电功能丧失",
+            "自我充电功能丧失",
+        ]
+
+        # error link
+        weibo_urls = [
+            "weibo.com/n/自我充电功能丧失",
+            "http://weibo.com/自我充电功能丧失",
+            "https://weibo.com/O8DM0BLLm/?test=123",
+            "https://weibo.com/userid/postid/",
+        ]
+
+        with pytest.raises(APINotFoundError):
+            await WeiboScreenNameFetcher.get_all_weibo_screen_name(weibo_urls)
