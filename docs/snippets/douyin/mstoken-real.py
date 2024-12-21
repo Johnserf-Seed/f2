a639e21cc13c4cdc8022e943e6653e963b0437d9
JohnserfSeed <johnserf-seed@foxmail.com>
2024-12-21 22:41:37 +0800

    docs: 删除多余代码片段

--- docs/snippets/douyin/mstoken-real.py
+++
@@ -1,4 +0,0 @@
-from f2.apps.douyin.utils import TokenManager
-
-if __name__ == "__main__":
-    print("douyin real msToken:", TokenManager.gen_real_msToken())
--- docs/snippets/douyin/s_v_web_id.py
+++
@@ -1,4 +0,0 @@
-from f2.apps.douyin.utils import VerifyFpManager
-
-if __name__ == "__main__":
-    print("verify_fp:", VerifyFpManager.gen_s_v_web_id())
