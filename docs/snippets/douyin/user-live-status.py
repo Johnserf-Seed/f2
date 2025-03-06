e13ae3b459241e9faa0c3225edfd06a87b2e4dfe
JohnserfSeed <johnserf-seed@foxmail.com>
2025-03-06 17:07:25 +0800

    docs: 添加抖音用户直播状态代码片段

---
+++ docs/snippets/douyin/user-live-status.py
@@ -0,0 +1,24 @@
+import asyncio
+from f2.apps.douyin.handler import DouyinHandler
+
+kwargs = {
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0",
+        "Referer": "https://www.douyin.com/",
+    },
+    "proxies": {"http://": None, "https://": None},
+    "cookie": "YOUR_COOKIE_HERE",
+}
+
+
+async def main():
+    user_id = "92908021676"
+    live_status = await DouyinHandler(kwargs).fetch_user_live_status(user_id=user_id)
+    # print("=================_to_raw================")
+    # print(live_status._to_raw())
+    print("=================_to_dict===============")
+    print(live_status._to_dict())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
